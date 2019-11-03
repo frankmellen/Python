d1d69384d17bc1026b6a70584fb27d642f0d1575
Frank <mellen.com@gmail.com>
2019-11-03 19:52:22 +0800

    add new file: function.py

---
+++ Python_Practice/function.py
@@ -0,0 +1,38 @@
+# funciton：函数
+# 基本语法：def (全称：define）下定义
+
+def function_name_1():
+    print("hello function")
+
+function_name_1()
+
+
+a=10
+b=20
+print(a+b)
+
+c=30
+d=40
+print(c+d)
+
+
+def sum(number1,number2):
+    print(number1+number2)
+
+sum(1,1)
+sum(100,200)
+
+
+def fun2():
+    return "hello function 2"
+
+a=fun2()
+print(a)
+
+
+
+def sum2(a,b):
+    return (a+b)
+
+sum=sum2(10,100)
+print(sum)
