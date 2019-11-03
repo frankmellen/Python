7353104ee4856d9871981556f360506934843699
Frank <mellen.com@gmail.com>
2019-11-03 19:21:06 +0800

    add new codes

--- Python_Practice/test 1.py
+++ Python_Practice/test 1.py
@@ -1,3 +1,17 @@
-print("hello,frank")
+test = ("hello,\nsecond line")
+print(test)
 
-print("add new code")
+words = ("first,\nsecond,\nthird")
+print(words)
+
+print("first 1" + ",\n" + "second 2" + "," + "third 3")
+
+print("test" * 100)
+
+print("|" * 300)
+
+print("=" * 300)
+
+print("0" * 300)
+
+print(300 * "8")
