3d04a49b6ef77ee08fdeec388fcedf13fb2a7ebf
Frank <mellen.com@gmail.com>
2019-11-03 16:38:44 +0800

    reformat code

--- Python_Practice/for_loop.py
+++ Python_Practice/for_loop.py
@@ -1,22 +1,10 @@
-
-
-
-
-
-
-
-
-
-
-
-
-mylist=[1,2,3,4,5,6,7,8,9,10]
+mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 print(mylist[0])
 print(mylist[1])
 print(mylist[2])
 
-mylist2=["jake","tom","mark"]
+mylist2 = ["jake", "tom", "mark"]
 
 for name in mylist2:
     print(name)
@@ -24,21 +12,19 @@ for name in mylist2:
 for number in range(10):
     print(number)
 
-for number in range(10,20):
+for number in range(10, 20):
     print(number)
 
-for number in range(10,20):
-    print(number+1)
+for number in range(10, 20):
+    print(number + 1)
 
-for number in range(10,21):
-    print(number+10)
+for number in range(10, 21):
+    print(number + 10)
 
 for letter in "frank":
     print(letter)
 
 for letter in "frank":
-    print("each letter is: "+letter)
+    print("each letter is: " + letter)
 
 print(type(letter))
-
-
