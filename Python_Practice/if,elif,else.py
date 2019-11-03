25593e08eea13435a0ba61e2e45c80d793489122
Frank <mellen.com@gmail.com>
2019-11-03 16:22:06 +0800

    add new codes

--- Python_Practice/if,elif,else.py
+++ Python_Practice/if,elif,else.py
@@ -1,8 +1,33 @@
-a = 10
-b = 20
+# a = 10
+# b = 20
+#
+# if b > a:
+#     print("hello, ok")
+#
+# if b < 30:
+#     print("hello, ng")
 
-if b > a:
-    print("hello, ok")
+# if
+# elif
+# else
 
-if b < 30:
-    print("hello, ng")
+
+
+# print(type(age))
+# <class 'str'>
+
+# age=input("hi, please enter your age: ")
+# print(type(age))
+
+
+age=int(input("hi, please enter your age: "))
+
+if age<18:
+    print("you can not smoke")
+
+elif age==18:
+    print("you are now 18, you can go to smoke")
+elif age==100:
+    print("hey!! you are 100 years old, please quit smoking")
+else:
+    print("you can smoke")
