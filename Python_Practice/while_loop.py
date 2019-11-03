5bdfbd18131180811b8f5c90db7e6764c3a7c133
Frank <mellen.com@gmail.com>
2019-11-03 19:35:52 +0800

    add new codes

---
+++ Python_Practice/while_loop.py
@@ -0,0 +1,12 @@
+condition = 1
+while condition < 10:
+    print(condition)
+    condition = condition + 2
+
+for a in range(1, 10):
+    print(a)
+
+while True:
+    a = int(input("please enter number a: "))
+    b = int(input("please enter number b: "))
+    print("a+b = " + str(a + b))
