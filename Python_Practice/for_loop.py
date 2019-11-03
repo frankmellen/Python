05fae7d48f3d481c4b6f108f47eeb4259aa42235
Frank <mellen.com@gmail.com>
2019-11-03 16:37:01 +0800

    new file: for_loop.py

---
+++ Python_Practice/for_loop.py
@@ -0,0 +1,44 @@
+
+
+
+
+
+
+
+
+
+
+
+
+mylist=[1,2,3,4,5,6,7,8,9,10]
+
+print(mylist[0])
+print(mylist[1])
+print(mylist[2])
+
+mylist2=["jake","tom","mark"]
+
+for name in mylist2:
+    print(name)
+
+for number in range(10):
+    print(number)
+
+for number in range(10,20):
+    print(number)
+
+for number in range(10,20):
+    print(number+1)
+
+for number in range(10,21):
+    print(number+10)
+
+for letter in "frank":
+    print(letter)
+
+for letter in "frank":
+    print("each letter is: "+letter)
+
+print(type(letter))
+
+
